4a4137d96e137a9a030886a417b5c350cb95ffca
ernestoacosta75 <39125962+ernestoacosta75@users.noreply.github.com>
2018-05-12 00:49:58 +0200

    Updating

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -11,6 +11,7 @@ import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
